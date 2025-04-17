c19483d89fe788709bf82ae04f0417f8b5a280b8
Phanishwar-pandey <136552319+Phanishwar-pandey@users.noreply.github.com>
2025-04-17 17:30:14 +0530

    Update Automatic_Plant_Watering_System.ino

--- Automatic_Plant_Watering_System.ino
+++ Automatic_Plant_Watering_System.ino
@@ -1,17 +1,13 @@
 #include "dht.h"
 #define dht_apin A0
-#define soilPin A1
 
 dht DHT;
 int water;
-int soilMoistureValue;
-int soilMoisturePercent;
 
 void setup() {
   Serial.begin(9600);
   pinMode(3, OUTPUT);
   pinMode(6, INPUT);
-  pinMode(soilPin, INPUT);
 
   delay(500);
   Serial.println("DHT11 Humidity & Temperature Sensor Initialized");
@@ -28,17 +24,12 @@ void loop() {
   Serial.println("C");
 
   water = digitalRead(6);
+
   if (water == HIGH) {
     digitalWrite(3, LOW);
   } else {
     digitalWrite(3, HIGH);
   }
-
-  soilMoistureValue = analogRead(soilPin);
-  soilMoisturePercent = map(soilMoistureValue, 1023, 0, 0, 100);
-  Serial.print("Soil moisture = ");
-  Serial.print(soilMoisturePercent);
-  Serial.println("%");
 
   delay(5000);
 }
